b1a9a144d2ae257853d00fc43c3eeff3689a0928
mourad_br <mouradbarkouch@gmail.com>
2022-07-02 13:46:12 +0100

    styling

--- app_0/lib/main.dart
+++ app_0/lib/main.dart
@@ -18,6 +18,7 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(),
       drawer: Drawer(),
       body: Text('Hello from HomePage.')
+
     );
   }
 }//UI: user interface.
@@ -27,12 +28,26 @@ class Login extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(),
       drawer: Drawer(),
-      body: Text('Hello from LoginPage.', 
-      style:TextStyle(
-        fontSize:30,
-        color:Color.fromARGB(255, 27, 87, 135),
-        fontWeight: FontWeight.bold,
-      )
+      body:  Container(
+        color:Colors.lightBlue,
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+                //EdgeInsets.only(right:10),
+                //EdgeInsets.inly(right:10,buttom:13,left:10 ,top:13),
+                //EdgeInsets.symetric(vertical:20,horizontal:12),
+        padding:EdgeInsets.all(20),
+        height: 100,
+        child: const Text('Hello from LoginPage.', 
+          style:TextStyle(
+            fontSize:30,
+            color:Color.fromARGB(255, 27, 87, 135),
+            fontWeight: FontWeight.bold,
+            letterSpacing:5.0,
+            wordSpacing: 2,
+            decoration:TextDecoration.lineThrough,//overline || underline || 
+            //backgroundColor:Colors.lightBlue,
+          )
+        )
       ),
     );
   }
