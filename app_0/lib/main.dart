06c3df409f5443d7be7273ebe389844d94e72502
mourad_br <mouradbarkouch@gmail.com>
2022-07-31 15:29:04 +0100

    local images

--- app_0/lib/main.dart
+++ app_0/lib/main.dart
@@ -28,26 +28,38 @@ class Login extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(),
       drawer: Drawer(),
-      body:  Container(
-        color:Colors.lightBlue,
+      body:  Container
+      (
+        //color:Colors.lightBlue, 
         width: double.infinity,
         margin: EdgeInsets.all(10),
                 //EdgeInsets.only(right:10),
                 //EdgeInsets.inly(right:10,buttom:13,left:10 ,top:13),
                 //EdgeInsets.symetric(vertical:20,horizontal:12),
-        padding:EdgeInsets.all(20),
-        height: 100,
+        padding:EdgeInsets.all(40),
+        height: double.infinity,
+        decoration:BoxDecoration(
+            color:Colors.lightBlue,
+            border:Border.all(color:Colors.black,width:2),
+            // border: Border(left: BorderSide(color: Colors.red, width:2), Border(right:BorderSide(color:Colors.green, width:2)))
+            // ignore: prefer_const_constructors
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              repeat: ImageRepeat.repeatX, // repeatY , noRepeat
+              image: const NetworkImage('https://pbs.twimg.com/media/D9n35y6WkAIaciR?format=jpg&name=large')),
+        ),
+
         child: const Text('Hello from LoginPage.', 
           style:TextStyle(
             fontSize:30,
-            color:Color.fromARGB(255, 27, 87, 135),
+            color:Color.fromARGB(255, 255, 255, 255),
             fontWeight: FontWeight.bold,
             letterSpacing:5.0,
             wordSpacing: 2,
             decoration:TextDecoration.lineThrough,//overline || underline || 
             //backgroundColor:Colors.lightBlue,
           )
-        )
+        ),
       ),
     );
   }
