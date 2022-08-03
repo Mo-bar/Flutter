469e3a8531274b58e5e304b95f8114db4bd4a895
mourad_br <mouradbarkouch@gmail.com>
2022-08-03 22:26:10 +0100

    wrap props.

--- app_0/lib/main.dart
+++ app_0/lib/main.dart
@@ -7,7 +7,7 @@ class Myapp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Stacks(),
+      home: Wrap_(),
     );
   }
 }
@@ -348,6 +348,51 @@ class Page_0 extends StatelessWidget{
           ],
         ),
       ),
+    );
+  }
+}
+//Wrap
+class Wrap_ extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      body: Wrap(
+        direction: Axis.horizontal,
+        children: [
+          Container(
+            margin: EdgeInsets.all(12),
+            color:Colors.red,
+            width:90,
+            height:100
+          ),          
+          Container(
+            margin: EdgeInsets.all(12),
+            color:Colors.red,
+            width:90,
+            height:100
+          ),          
+          Container(
+            margin: EdgeInsets.all(12),
+            color:Colors.red,
+            width:90,
+            height:100
+          ),          
+          Container(
+            margin: EdgeInsets.all(12),
+            color:Colors.red,
+            width:90,
+            height:100
+          ),
+            Container(
+            margin: EdgeInsets.all(12),
+            color:Colors.red,
+            width:90,
+            height:100
+          )
+        ],
+      )
     );
   }
 }
