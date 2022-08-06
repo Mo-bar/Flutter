ffacfd1fa4f41298de0b1e7e118d2aa51e218d6c
mourad_br <mouradbarkouch@gmail.com>
2022-08-06 23:11:27 +0100

    CheckboxListTile properties.

--- app_0/lib/check_box.dart
+++ app_0/lib/check_box.dart
@@ -7,41 +7,92 @@ class Check_box extends StatefulWidget {
 }
 
 class _Check_boxState extends State<Check_box> {
-  bool female = false, male = false;
+  bool female = false, male = false, usa = false, ma = false;
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(),
       drawer: Drawer(),
       body: Container(
-        height: 100,
-        width: 300,
         padding: EdgeInsets.all(20),
         margin: EdgeInsets.all(20),
-        child: Row(children: [
-          Text('Gender'),
-          // VerticalDivider(color: Colors.white,thickness: 4,),
-          Spacer(),
-          Text('Female'),
-          Checkbox(
-            activeColor: Colors.black, //bg check color.
-            checkColor: Colors.white, // check color.
-            value: female, 
-            onChanged: (val){
-              setState(() {
-                female = val!;
-              });
-              }),
-              Text('Male'),
-              Checkbox(
-                value: male, 
-                onChanged: (val){
+        child: Column(
+          children: 
+          [
+            Container( alignment: Alignment.topLeft,child: Text('Checkbox.',style: TextStyle(fontWeight: FontWeight.bold),)),
+            Container(
+              child: Row(children: 
+              [
+                Text('Gender'),Spacer(),
+                // VerticalDivider(color: Colors.white,thickness: 4,),
+                // Spacer(),
+                Text('Female'),
+                Checkbox(
+                  activeColor: Colors.black, //bg check color.
+                  checkColor: Colors.white, // check color.
+                  value: female, 
+                  onChanged: (val){
+                    setState(() {
+                      female = val!;
+                    });
+                    }),
+                    Text('Male'),
+                Checkbox(
+                      value: male, 
+                      onChanged: (val){
+                        setState(() {
+                          male = val!;
+                        });
+                      }),
+              ]),
+            ),
+          
+          Divider(thickness:3),
+          Container(
+            child: Column(
+              children: [
+                Container(alignment: Alignment.topLeft,child: Text('ChechboxListTile', style: TextStyle(fontWeight: FontWeight.bold),)),
+                CheckboxListTile(
+                  contentPadding: EdgeInsets.only(top:10),
+                  title: Text('USA'),
+                  subtitle: Text('United State of America.'),
+                  secondary: Icon(Icons.flag),
+                  checkColor: Colors.white,
+                  activeColor: Colors.lightBlueAccent,
+                  isThreeLine: true,
+                  value: usa,
+                  selected:usa,
+                  controlAffinity: ListTileControlAffinity.platform,
+                  onChanged:((val){ 
                   setState(() {
-                    male = val!;
+                    usa = val!;
+                  });
+                  })
+                ),
+                CheckboxListTile(
+                  contentPadding: EdgeInsets.only(top:10),
+                  title: Text('Morocco'),
+                  subtitle: Text('Kingdom of Morocco.'),
+                  secondary: Icon(Icons.flag),
+                  checkColor: Colors.white,
+                  activeColor: Colors.redAccent,
+                  isThreeLine: true,
+                  value: ma,
+                  selected:ma,
+                  controlAffinity: ListTileControlAffinity.leading,
+                  onChanged:((val){ 
+                  setState(() {
+                    ma = val!;
                   });
                 })
-        ],),
-      ),
+                )
+              ],
+            ),
+          )
+                // CheckboxListTile(value: , onChanged: onChanged)
+          ],
+        )
+    ),
     );
   }
 }
