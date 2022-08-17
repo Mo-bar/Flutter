370be39f306bd5e97bf47b7afbb52486b28fb669
mourad_br <mouradbarkouch@gmail.com>
2022-08-17 18:13:55 +0100

    get needed flutter pub for firebase

--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 // import 'package:dropdown_search/dropdown_search.dart';
 import 'Auth/Homepage.dart';
 import 'Auth/Login.dart';
 import 'Auth/SignUp.dart';
 import 'CRUD/addNotes.dart';
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( MyApp());
 }
 
