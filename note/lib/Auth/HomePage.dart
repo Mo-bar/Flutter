2045380ebdc3f20d3e28e74e976015c546f6bb87
mourad_br <mouradbarkouch@gmail.com>
2022-08-20 17:20:47 +0100

    remove unused container

--- note/lib/Auth/HomePage.dart
+++ note/lib/Auth/HomePage.dart
@@ -33,29 +33,27 @@ class _NotesState extends State<Notes> {
           snapshot.hasData ? ListView.builder(
                               itemCount: snapshot.data!.length,
                               itemBuilder: (context, i){
-                                return Container(
-                                  child: Dismissible(
-                                    key: Key('$i'),
-                                    direction: DismissDirection.horizontal,
-                                    onDismissed: (direction) async{
-                                      int resp = await sql.deleteData("DELETE FROM notes WHERE id = ${snapshot.data[i]['id']}");
-                                    },
-                                    child: Card(
-                                      child:Row(children: [
-                                        Expanded(
-                                          flex:1,
-                                          child: Image.asset('images/1.png', fit: BoxFit.fill,),
-                                        ),
-                                        Expanded(
-                                          flex:3,
-                                        child:  ListTile(
-                                          title: Text(snapshot.data[i]['title']),
-                                          subtitle: Text(snapshot.data[i]['note']),
-                                          trailing: IconButton(icon: Icon(Icons.edit), onPressed: (){}),
-                                        ),
-                                        )
-                                      ],)
-                                    ),
+                                return Dismissible(
+                                  key: Key('$i'),
+                                  direction: DismissDirection.horizontal,
+                                  onDismissed: (direction) async{
+                                    int resp = await sql.deleteData("DELETE FROM notes WHERE id = ${snapshot.data[i]['id']}");
+                                  },
+                                  child: Card(
+                                    child:Row(children: [
+                                      Expanded(
+                                        flex:1,
+                                        child: Image.asset('images/1.png', fit: BoxFit.fill,),
+                                      ),
+                                      Expanded(
+                                        flex:3,
+                                      child:  ListTile(
+                                        title: Text(snapshot.data[i]['title']),
+                                        subtitle: Text(snapshot.data[i]['note']),
+                                        trailing: IconButton(icon: Icon(Icons.edit), onPressed: (){}),
+                                      ),
+                                      )
+                                    ],)
                                   ),
                                 );
                                   // return ListNotes(notes: nots[0]['note']);
